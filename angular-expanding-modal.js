509f71559906546acd4d223346716159d4a51fdb
tomasz stryjewski <stryjewski.t@gmail.com>
2015-07-22 10:06:25 +0200

    phase 1

--- angular-expanding-modal.js
+++ angular-expanding-modal.js
@@ -10,298 +10,223 @@
   'use strict';
 
   function ExpandingModal( $compile, $rootScope, $document, $controller, $q, $http, $templateCache ) {
-    return {
-      /**
-       * Factory function to create new modal instance
-       *
-       * @param  {Object} config config
-       * @return {Object}        modal
-       */
-      create : config => {
+
+    return class Modal {
+      constructor( config = {} ) {
         if ( ! ( ! config.template ^ ! config.templateUrl ) ) {
           throw new Error(
             'Expected modal to have exacly one of either `template` or `templateUrl`'
           );
         }
 
-        let container     = ng.element( config.container || $document[ 0 ].body );
-        let controller    = config.controller || null;
-        let controllerAs  = config.controllerAs;
-        let element       = null;
-        let html          = _getHtmlString( config );
-        let target;
-        let scope;
+        config.container = ng.element( config.container || $document[ 0 ].body );
 
+        this._config = config;
+      }
 
-        /**
-         * Evlaluate config and check if html-string
-         * is included and if not fetch html string
-         * via $http
-         *
-         * @param  {Object}         config config
-         * @return {String|Promise}        promise resolving with
-         *                                 html string
-         */
-        function _getHtmlString( config ) {
-          if ( config.template ) {
-            html = $q.when( config.template );
-          } else {
-            html = $http.get( config.templateUrl, {
-              cache : $templateCache
-            } ).then( ( { data } ) => data );
-          }
+      open( target, locals ) {
+        return $q.when( Modal.html( this.config ) )
+          .then( ( html ) => Modal.attach( this, html, target, locals ) );
+      }
 
-          return html;
+      close () {
+        if ( ! this._element ) {
+          return $q.when();
         }
 
+        return Modal.detach( this._target, this._element )
+          .then( () => Modal.cleanup( this ) );
+      }
 
-        /**
-         * Helper function to set property with
-         * vendor prefixes to elemnets
-         *
-         * @param {Object} element DOM element
-         * @param {Sring}  prop    property name
-         * @param {String} value   property value
-         */
-        function _setPrefixedProperty( element, prop, value ) {
-          element.style[ prop ] = value;
-          element.style[ '-webkit-' + prop ] = value;
+
+      /**
+       * Evlaluate config and check if html-string
+       * is included and if not fetch html string
+       * via $http
+       *
+       * @param  {Object}  config config object
+       * @return {Promise}        promise resolving with
+       *                                 html string
+       */
+      static html( { template, templateUrl } ) {
+        if ( template ) {
+          return template;
         }
 
+        return $http.get( templateUrl, {
+          cache : $templateCache
+        } )
+          .then( ( { data } ) => data );
+      }
 
-        /**
-         * Attach the modal to the DOM
-         *
-         * @param  {String} html       html
-         * @param  {Object} targetElem target element
-         * @param  {Object} locals     locals
-         */
-        function attach( html, targetElem, locals = {} ) {
-          let defer = $q.defer();
+      static attach( modal, html, target, locals ) {
+        if ( modal._element ) {
+          return modal._element;
+        }
 
-          target = targetElem;
+        modal._target = target;
 
-          element = ng.element( html );
+        modal._element = Modal.createElement( html )
+          .then( ( element ) => Modal.compile( modal, element, locals ) )
+          .then( ( element ) => Modal.append( target, element ) );
+      }
 
-          if ( element.length === 0 ) {
-            throw new Error(
-              'The template contains no elements; you need to wrap text nodes'
-            );
+      static createElement( html ) {
+        const element = ng.element( html );
+
+        if ( element.length === 0 ) {
+          throw new Error(
+            'The template contains no elements; you need to wrap text nodes'
+          );
+        }
+
+        return element;
+      }
+
+      static compile( modal, element, locals = {} ) {
+        const scope = $rootScope.$new();
+        const { controller, controllerAs } = modal._config;
+
+        if ( controller ) {
+          locals.$scope = scope;
+
+          const ctrl = $controller( controller, locals );
+
+          if ( controllerAs ) {
+            scope[ controllerAs ] = ctrl;
           }
-          scope = $rootScope.$new();
-
-          if ( controller ) {
-            locals.$scope = scope;
-
-            var ctrl = $controller( controller, locals );
-
-            if ( controllerAs ) {
-              scope[ controllerAs ] = ctrl;
-            }
-          } else if ( locals ) {
-            for ( let prop in locals ) {
-              scope[ prop ] = locals[ prop ];
-            }
+        } else if ( locals ) {
+          for ( let prop in locals ) {
+            scope[ prop ] = locals[ prop ];
           }
+        }
 
-          $compile( element )( scope );
+        modal._scope = scope;
 
-          element[ 0 ].style.opacity = 0;
+        return $compile( element )( scope );
+      }
 
-          container.append( element[ 0 ] );
+      static append( target, [ element ] ) {
+        const deferred = $q.defer();
 
-          let startRect = target.getBoundingClientRect();
-          let endRect   = element[ 0 ].getBoundingClientRect();
+        const startRect = target.getBoundingClientRect();
+        const endRect   = element.getBoundingClientRect();
 
-          let startValues = {
-            top  : ( startRect.top + window.scrollY ),
-            left : startRect.left,
+        startRect.top += window.scrollY;
+        endRect.top   += window.scrollY;
 
-            height : startRect.height,
-            width  : startRect.width
-          };
+        const transformRule = Modal.getTransformRule( element, startRect, endRect );
 
-          let endValues = {
-            top  : ( endRect.top + window.scrollY ),
-            left : endRect.left,
+        const listenerFunc = function () {
+          element.classList.remove( 'transitionOut' );
+          element.classList.add( 'done' );
+          Modal.setPrefixedProperty( element, 'transform', '' );
+          Modal.setPrefixedProperty( element, 'transformOrigin', '' );
+          element.style.opacity   = '';
 
-            height : endRect.height,
-            width  : endRect.width
-          };
+          element.removeEventListener( 'transitionend', listenerFunc );
 
-          let transformDiff = {
-            x : 0,
-            y : 0
-          };
+          deferred.resolve( arguments[ 1 ] );
+        };
 
-          let computedStyle   = window.getComputedStyle( element[ 0 ], null );
-          let transformStyles = computedStyle.getPropertyValue( 'transform' ) ||
-                                window.getComputedStyle( element[ 0 ], null ).getPropertyValue( '-webkit-transform' );
+        requestAnimationFrame( function () {
+          Modal.setPrefixedProperty( element, 'transform', transformRule );
+          Modal.setPrefixedProperty( element, 'transformOrigin', '0 0' );
 
+          element.style.opacity         = 0.25;
 
-          if ( transformStyles && transformStyles !== 'none' ) {
-            let transformValues = transformStyles.replace( /(matrix\(|\))/g, '' ).split( ',' );
+          requestAnimationFrame( function () {
+            element.addEventListener( 'transitionend', listenerFunc, false );
 
-            transformDiff.x = + transformValues[ 4 ];
-            transformDiff.y = + transformValues[ 5 ];
-          }
+            element.classList.add( 'transitionOut' );
 
-          let scaleX = startValues.width / endValues.width;
-          let scaleY = startValues.height / endValues.height;
-
-          let transformRule = 'translate3d(' +
-                                ( startValues.left - endValues.left + transformDiff.x ) + 'px, ' +
-                                ( startValues.top - endValues.top + transformDiff.y ) + 'px,' +
-                                '0 ) ' +
-                              'scale(' + scaleX + ',' + scaleY + ')';
-
-          var listenerFunc = function listener() {
-            element[ 0 ].classList.remove( 'transitionOut' );
-            element[ 0 ].classList.add( 'done' );
-            _setPrefixedProperty( element[ 0 ], 'transform', '' );
-            _setPrefixedProperty( element[ 0 ], 'transformOrigin', '' );
-            element[ 0 ].style.opacity   = '';
-
-
-            element[ 0 ].removeEventListener( 'transitionend', listenerFunc );
-
-            defer.resolve();
-          };
-
-
-          requestAnimationFrame( () => {
-            _setPrefixedProperty( element[ 0 ], 'transform', transformRule );
-            _setPrefixedProperty( element[ 0 ], 'transformOrigin', '0 0' );
-
-            element[ 0 ].style.opacity         = 0.25;
-
-            requestAnimationFrame( () => {
-
-              element[ 0 ].addEventListener( 'transitionend', listenerFunc, false );
-
-              element[ 0 ].classList.add( 'transitionOut' );
-
-              _setPrefixedProperty( element[ 0 ], 'transform', '' );
-              _setPrefixedProperty( element[ 0 ], 'transformOrigin', '.5 .5' );
-              element[ 0 ].style.opacity   = 1;
-            } );
+            Modal.setPrefixedProperty( element, 'transform', '' );
+            Modal.setPrefixedProperty( element, 'transformOrigin', '.5 .5' );
+            element.style.opacity   = 1;
           } );
+        } );
+      }
 
-          return defer.promise;
-        }
+      static detach( target, [ element ] ) {
+        const deferred = $q.defer();
 
+        const startRect = element.getBoundingClientRect();
+        const endRect   = target.getBoundingClientRect();
 
-        /**
-         * Open the modal
-         *
-         * @param  {Object} target target element
-         * @param  {Object} locals locals
-         */
-        function open( target, locals ) {
-          let defer = $q.defer();
+        const transformRule = Modal.getTransformRule( element, startRect, endRect );
 
-          html.then( html => {
-            if ( ! element ) {
-              attach( html, target, locals ).then( defer.resolve );
-            }
-          } );
+        element.style.opacity = 1;
 
-          return defer.promise;
-        }
+        const listenerFunc = function listener() {
+          element.classList.remove( 'transitionIn' );
+          element.classList.add( 'done' );
+          element.style.opacity = 0;
 
+          element.removeEventListener( 'transitionend', listenerFunc, false );
 
-        /**
-         * Close the modals and remove it from the DOM
-         */
-        function close() {
-          if ( ! element ) {
-            return $q.when();
-          }
+          deferred.resolve();
+        };
 
-          let defer = $q.defer();
+        requestAnimationFrame( () => {
+          element.classList.add( 'transitionIn' );
+          element.classList.remove( 'done' );
 
-          let startRect = element[ 0 ].getBoundingClientRect();
-          let endRect   = target.getBoundingClientRect();
+          Modal.setPrefixedProperty( element, 'transform', transformRule );
+          Modal.setPrefixedProperty( element, 'transformOrigin', '0 0 ' );
 
-          let startValues = {
-            top  : startRect.top,
-            left : startRect.left,
+          element.style.opacity   = 0;
 
-            height : startRect.height,
-            width  : startRect.width
-          };
+          element.addEventListener( 'transitionend', listenerFunc, false );
+        } );
 
-          let endValues = {
-            top  : endRect.top,
-            left : endRect.left,
+        return deferred.promise;
+      }
 
-            height : endRect.height,
-            width  : endRect.width
-          };
+      static cleanup( modal ) {
+        modal._scope.$destroy();
+        modal._scope = null;
 
-          let transformDiff = {
-            x : 0,
-            y : 0
-          };
+        modal._element.remove();
+        modal._element = null;
 
-          let computedStyle   = window.getComputedStyle( element[ 0 ], null );
-          let transformStyles = computedStyle.getPropertyValue( 'transform' ) ||
+        modal._target  = null;
+
+        return true;
+      }
+
+      static setPrefixedProperty( element, prop, value ) {
+        element.style[ prop ] = value;
+        element.style[ '-webkit-' + prop ] = value;
+      }
+
+      static getTransformRule( element, startRect, endRect ) {
+        const transformDiff = {
+          x : 0,
+          y : 0
+        };
+
+        const computedStyle   = window.getComputedStyle( element, null );
+        const transformStyles = computedStyle.getPropertyValue( 'transform' ) ||
                                 computedStyle.getPropertyValue( '-webkit-transform' );
 
-          if ( transformStyles && transformStyles !== 'none' ) {
-            let transformValues = transformStyles.replace( /(matrix\(|\))/g, '' ).split( ',' );
+        if ( transformStyles && transformStyles !== 'none' ) {
+          const [,,,, x, y ] = transformStyles.replace( /(matrix\(|\))/g, '' ).split( ',' );
 
-            transformDiff.x = + transformValues[ 4 ];
-            transformDiff.y = + transformValues[ 5 ];
-          }
-
-          let transformRule = 'translate3d(' +
-                                ( endValues.left - startValues.left + transformDiff.x ) + 'px , ' +
-                                ( endValues.top - startValues.top +  transformDiff.y ) + 'px,' +
-                                '0 )' +
-                              'scale(' +
-                                ( endValues.width / startValues.width ) + ',' +
-                                ( endValues.height / startValues.height ) + ')';
-
-          element[ 0 ].style.opacity = 1;
-
-          let listenerFunc = function listener() {
-            element[ 0 ].classList.remove( 'transitionIn' );
-            element[ 0 ].classList.add( 'done' );
-            element[ 0 ].style.opacity = 0;
-
-            element[ 0 ].removeEventListener( 'transitionend', listenerFunc, false );
-
-
-            scope.$destroy();
-            scope = null;
-
-            element.remove();
-            element = null;
-            target  = null;
-
-            defer.resolve();
-          };
-
-          requestAnimationFrame( () => {
-            element[ 0 ].classList.add( 'transitionIn' );
-            element[ 0 ].classList.remove( 'done' );
-
-            _setPrefixedProperty( element[ 0 ], 'transform', transformRule );
-            _setPrefixedProperty( element[ 0 ], 'transformOrigin', '0 0 ' );
-
-            element[ 0 ].style.opacity   = 0;
-
-            element[ 0 ].addEventListener( 'transitionend', listenerFunc, false );
-          } );
-
-          return defer.promise;
+          transformDiff.x = + x;
+          transformDiff.y = + y;
         }
 
-        return {
-          open  : open,
-          close : close
+        const scale = {
+          x : startRect.width / endRect.width,
+          y : startRect.height / endRect.height
         };
+
+        const translate = {
+          x : startRect.left - endRect.left + transformDiff.x,
+          y : startRect.top - endRect.top + transformDiff.y
+        };
+
+        return `translate3d( ${translate.x}px,${translate.y}px,0 ) scale( ${scale.x},${scale.y} )`;
       }
     };
   }
